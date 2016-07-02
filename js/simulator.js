13cd692a531e16dda4b95d20896960dac9e9aab0
Kenny Vu <Kenny Vu>
2016-07-02 15:35:37 +0100

    The prototype onclick function now displays the numbers in the lottery

--- js/simulator.js
+++ js/simulator.js
@@ -1,8 +1,22 @@
 //This function is executed when the submit button is clicked
 function appendNumbers(){
-	var max = $('#txt_number').val();
+	//MAX NUMBER IS 84, otherwise the page formats gets ruined
+	var max = $('#txt_number').val(); //
 	
+	if(max >84){
+		alert("This number is too big");
+		return;
+	}
+	
+	
+	var counter = 0;
+	
+	$('.number_box').remove();
+	$('br').remove();
 	for(i=1;i<=max;i++){
-		$('#main').append("<p style='display:inline;'>Test</p>");
+		counter++;
+		$('#main').append("<div class='number_box'> <span class='number'>" + i + " </span></div>");
+		if(counter%14 == 0)
+			$('#main').append("</br>");
 	}
 }
